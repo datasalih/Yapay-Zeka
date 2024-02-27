df4aeb371ad1242577ba879dfdcc4cc26efc043e
datasalih <salihsenova01@gmail.com>
2024-02-27 22:09:34 +0300

    1

--- chatbot.py
+++ chatbot.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import google.generativeai as genai
 
-genai.configure(api_key="YOUR_API_KEY")
+genai.configure(api_key="AIzaSyCpqldIOjklqL1c4nsOgyyXHUQR0wabQY4")
 
 model = genai.GenerativeModel(model_name="gemini-pro")
 
